36c7d62eb1643c9c304048e781b8b531c487a738
Hydroxide <hydroxygen50@gmail.com>
2019-08-01 20:52:01 +0800

    cross validation with groups

--- project/groupkfold.py
+++ project/groupkfold.py
@@ -1,18 +1,15 @@
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import KFold,GroupKFold,LeaveOneGroupOut
-#from sklearn.SVM import SVC
+from sklearn.model_selection import KFold,GroupKFold,LeaveOneGroupOut,cross_val_score
+from sklearn.svm import SVC
 n_folds=3
-store=pd.HDFStore('train_pca.h5')
+store=pd.HDFStore('train_data.h5')
 pca_data=store['data']
 labels=store['labels']
 access=store['accessions']
 store.close()
-print(pca_data.shape)
-print(labels.shape)
-print(access.shape)
 
-#cv=KFold(3).split(pca_data)
+#cv=KFold(n_folds).split(pca_data)
 cv=GroupKFold(n_folds).split(pca_data,labels,access)
 #cv=LeaveOneGroupOut().split(pca_data,labels,access)
 for train_index , test_index in cv:
@@ -21,11 +18,6 @@ for train_index , test_index in cv:
     print('Train groups {}'.format(len(np.unique(access[train_index]))))
     print('Test groups {}'.format(len(np.unique(access[test_index]))))
     print()
-
-
-
-#Classification: SVC Linear Kernel; 5-Fold validation
-#"""model = SVC(kernel='linear', probability=True)
-#kfs = KFold(n_folds, shuffle=True, random_state=0).get_n_splits(rpkm_scaled)
-#scores = cross_val_score(model, rpkm_scaled, label, scoring='accuracy', cv = kfs)
-#print(scores)
+model=SVC(kernel='linear')
+scores=cross_val_score(model,pca_data,labels,access,cv=GroupKFold(n_folds),scoring='accuracy')
+print(scores)
